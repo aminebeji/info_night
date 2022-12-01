d24a328f26feacf2e15814c61c1f3b923b8cb899
aminebeji <aminebeji007@gmail.com>
2022-12-01 23:04:09 +0100

    add question and stages

--- src/store/quiz/reducer.js
+++ src/store/quiz/reducer.js
@@ -1,7 +1,26 @@
 import Types from "./Types";
 const inistialState = {
-  Question: [],
-  Stages: [],
+  Questions: [
+    { id: 1, question: "", stageID: 1, type: "oui/non", response: "oui" },
+    {
+      id: 2,
+      question: "question 1 ?",
+      stageID: 1,
+      type: "text",
+      response: "response here !",
+    },
+    {
+      id: 2,
+      question: "question 2 ?",
+      stageID: 2,
+      type: "text",
+      response: "response here !",
+    },
+  ],
+  Stages: [
+    { id: 1, name: "Stage 1", description: "" },
+    { id: 2, name: "Stage 2", description: "" },
+  ],
   currentStage: 0,
 };
 
